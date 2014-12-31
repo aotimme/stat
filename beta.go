4930655044ab37cd4a1bcdd1fec4c919d4a994f2
Alden Timme <alden@datafox.co>
2014-12-30 16:53:10 -0800

    use dirichlet for beta

--- beta.go
+++ beta.go
@@ -6,27 +6,24 @@ import (
 )
 
 type Beta struct {
-  // could just use Dirichlet
-  a, b float64
+  dir Dirichlet
 }
 
 func NewBeta(a, b float64) (beta Beta) {
-  beta.a = a
-  beta.b = b
+  beta.dir = NewDirichlet([]float64{a, b})
   return
 }
 
 func (beta *Beta) Sample(r *rand.Rand) float64 {
-  gammaA, gammaB := NewGamma(beta.a, 1),  NewGamma(beta.b, 1)
-  a, b := gammaA.Sample(r), gammaB.Sample(r)
-  return a / (a + b)
+  return beta.dir.Sample(r)[0]
 }
 
 func (beta *Beta) LogDensity(x float64) float64 {
   if x < 0 || x > 1 {
     return math.Inf(-1)
   }
-  return (beta.a - 1.0) * math.Log(x) + (beta.b - 1.0) * math.Log(1.0 - x)  + LogGamma(beta.a + beta.b) - LogGamma(beta.a) - LogGamma(beta.b)
+  xx := []float64{x, 1.0 - x}
+  return beta.dir.LogDensity(xx)
 }
 
 func (beta *Beta) Density(x float64) float64 {
